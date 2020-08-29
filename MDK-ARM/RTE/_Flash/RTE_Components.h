a87850e92b81d480293b3977343aa37f38d389e9
romancehl2 <heziyuanosu@hotmail.com>
2020-08-29 17:37:20 +0800

    123

---
+++ MDK-ARM/RTE/_Flash/RTE_Components.h
@@ -0,0 +1,31 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'hello-world' 
+ * Target:  'Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h7xx.h"
+
+/*  Keil::Device:STM32Cube HAL:Common:1.6.0 */
+#define RTE_DEVICE_HAL_COMMON
+/*  Keil::Device:STM32Cube HAL:Cortex:1.6.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/*  Keil::Device:STM32Cube HAL:GPIO:1.6.0 */
+#define RTE_DEVICE_HAL_GPIO
+/*  Keil::Device:STM32Cube HAL:PWR:1.6.0 */
+#define RTE_DEVICE_HAL_PWR
+/*  Keil::Device:STM32Cube HAL:RCC:1.6.0 */
+#define RTE_DEVICE_HAL_RCC
+
+
+#endif /* RTE_COMPONENTS_H */
